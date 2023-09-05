92bd4abb21d32e21f776332ca8ee20b6dd83b9e6
Ben Rehmann <brehmann@me.com>
2023-09-05 12:24:40 -0500

    could tweak

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -10,8 +10,15 @@ console.log('***** Object Practice *****')
 */
 const me = {
   // TODO - add properties here
+  firstName: `ben`,
+  lastName: `rehmann`,
+  hasSiblings: true,
+  shoeCount: 4,
+  favThreeFoods: [`oatmeal`, `bbq`, `pasta`]
 };
 console.log('A little about me:', me);
+
+me.numSiblings = 2;
 
 /* 2. Accessing object properties.
   - Create a variable called fullName 
@@ -19,13 +26,16 @@ console.log('A little about me:', me);
     created above to set its value. 
   - Console.log fullName
 */
-
+let fullName = `${me.firstName} ${me.lastName}`;
+console.log(fullName);
 
 
 /* 3. Nested arrays
   - Console.log your first favorite food
   - Console.log your last favorite food 
 */
+console.log(me.favThreeFoods[0]);
+console.log(me.favThreeFoods[me.favThreeFoods.length - 1]);
 
 
 
@@ -35,6 +45,8 @@ console.log('A little about me:', me);
   - Set your object's shoeCount to be what it was plus 1
   - Console.log your updated shoe count. 
 */
+me.shoeCount = 2;
+console.log(me.shoeCount);
 
 
 /* 5. Add a new property to an existing object.
@@ -43,3 +55,31 @@ console.log('A little about me:', me);
   - Cool huh? It works, even though it wasn't there before
   - Console.log your object
 */
+
+me.favoriteColor = `red`;
+console.log(me.favoriteColor);
+
+console.log(me);
+
+const meInfo = document.querySelector(`#me-info`);
+
+function appendToDom(obj){
+  // const meInfo = document.querySelector(`#me-info`);
+
+
+  meInfo.innerHTML += `<div> Hi! My name is ${obj.firstName} ${obj.lastName} </div>`;
+  meInfo.innerHTML += `<div> The color ${obj.favoriteColor} is my favorite </div>`;
+  meInfo.innerHTML += `<div> I have ${obj.shoeCount} shoes in my collection </div>`;
+
+  // siblings if statement
+  if( obj.hasSiblings === true ){
+    meInfo.innerHTML += `<div> I have ${obj.numSiblings} siblings! </div>`;
+  }// end siblings
+
+  //fav foods loop
+  for(let i = 0; i < me.favThreeFoods.length; i++){
+    meInfo.innerHTML += `<div> one of my favorite foods is ${me.favThreeFoods[i]} </div>`;
+  }// end loop of foods
+
+}// end function
+appendToDom(me);
